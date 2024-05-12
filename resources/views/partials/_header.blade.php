2541d07a7457027db708b96d072834c6b6eb1511
Faizan Majid <faizanmajid.20@gmail.com>
2024-05-12 17:47:47 +0500

    website logo implementation
    Website logo set and login button added.

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,15 +1,22 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-md navbar-dark navbar-custom fixed-top">
     <!-- Image Logo -->
-    <a class="navbar-brand logo-image" href="index.html">Abdullah</a>
-    
+    <a class="navbar-brand page-scroll" href="#header">
+        <img src="{{asset('images/MitoVison.png')}}" alt="Logo" height="60">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    {{-- <a class="navbar-brand logo-image" href="index.html">Abdullah</a> --}}
+
     <!-- Mobile Menu Toggle Button -->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-awesome fas fa-bars"></span>
         <span class="navbar-toggler-awesome fas fa-times"></span>
     </button>
     <!-- end of mobile menu toggle button -->
-
+    
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
@@ -32,15 +39,18 @@
             <li class="nav-item">
                 <a class="nav-link page-scroll" href="#footer">CONTACT</a>
             </li>
-
-            <li class="nav-item dropdown">
-                @if (Auth::check())
+            @if (!Auth::check())
+                <li class="nav-item">
+                    <a class="nav-link page-scroll" href="{{route('login')}}">LOGIN</a>
+                </li>
+            @endif
+            <li class="nav-item dropdown">     
+                @if(Auth::check())
+                <div>
                     <a class="nav-link dropdown-toggle page-scroll" href="#profile" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">
                         <img class="img-profile rounded-circle" src="{{asset('images/undraw_profile.svg')}}" style="width: 30; height: 30px;">
                     </a>
-                @endif
-                
-                @if(Auth::check())
+                </div>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="{{ route('doctor.dashboard') }}"><span class="item-text">Dashboard</span></a>
                    
